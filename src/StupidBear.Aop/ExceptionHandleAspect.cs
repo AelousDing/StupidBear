b789574f4a278c3a056bfecfe41a26d0c9973aa7
Aelous <djmjif@gmail.com>
2024-04-01 16:23:19 +0800

    集成Aop
    集成Aop

---
+++ src/StupidBear.Aop/ExceptionHandleAspect.cs
@@ -0,0 +1,21 @@
+﻿using MethodBoundaryAspect.Fody.Attributes;
+using Microsoft.Extensions.Logging;
+using StupidBear.Core.Ioc;
+
+namespace StupidBear.Fody
+{
+    public class ExceptionHandleAspect : OnMethodBoundaryAspect
+    {
+        private Type logType;
+        public ExceptionHandleAspect(Type logType)
+        {
+            this.logType = logType;
+
+        }
+        public override void OnException(MethodExecutionArgs arg)
+        {
+            var logger = ContainerLocator.Current.GetService(logType) as ILogger;
+            logger?.LogError($"{arg.Instance.GetType()} {arg.Method.Name}", arg.Exception);
+        }
+    }
+}
